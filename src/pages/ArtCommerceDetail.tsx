6c35e7fcfdef66283821d2dd6ebfaed63bb0081b
Miora-Harilaza <mioramh@gmail.com>
2025-10-31 13:23:48 -0800

    ok

--- src/pages/ArtCommerceDetail.tsx
+++ src/pages/ArtCommerceDetail.tsx
@@ -349,14 +349,6 @@ const ArtCommerceDetail = () => {
     );
   };
 
-
-
-
-
-
-
-
-
   const DeliveryForm = () => (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div className="bg-white rounded-xl p-6 max-w-md w-full border border-gray-200 shadow-2xl relative">
@@ -454,7 +446,7 @@ const ArtCommerceDetail = () => {
                 <X size={24} />
               </button>
             </div>
-      
+
 
             {/* Contenu principal */}
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
